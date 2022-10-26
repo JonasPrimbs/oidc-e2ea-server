c95568b4f4794f4256378e982d183d28161b03a9
Jonas Primbs <mail@jonasprimbs.de>
2022-10-26 14:28:26 +0200

    cleanup

--- generate-secrets.sh
+++ generate-secrets.sh
@@ -6,6 +6,7 @@ OP_USERNAME_FILE="${SECRETS_DIR}op_username.txt"
 OP_PASSWORD_FILE="${SECRETS_DIR}op_password.txt"
 RIDT_ENV_FILE="${SECRETS_DIR}ridt.env"
 OP_ENV_FILE="${SECRETS_DIR}op.env"
+ENV_FILE=".env"
 
 # $1 = file name
 # $2 = secret length
@@ -25,16 +26,21 @@ function generate_secret {
 # Create DB password secret file if not exists
 [ ! -f "$DB_PASSWORD_FILE" ] && generate_secret "$DB_PASSWORD_FILE" 64
 
-# Write DB username + password to op.env file
-[ ! -f "$OP_ENV_FILE" ] && echo "KC_DB_USERNAME=$(cat $DB_USERNAME_FILE)" > "$OP_ENV_FILE" && echo "KC_DB_PASSWORD=$(cat $DB_PASSWORD_FILE)" >> "$OP_ENV_FILE"
-
 # Create OP username secret file if not exists
 [ ! -f "$OP_USERNAME_FILE" ] && generate_secret "$OP_USERNAME_FILE" 14
 
 # Create OP password secret file if not exists
 [ ! -f "$OP_PASSWORD_FILE" ] && generate_secret "$OP_PASSWORD_FILE" 64
 
+# Write DB username + password to op.env file
+[ ! -f "$OP_ENV_FILE" ] && echo "KC_DB_USERNAME=$(cat $DB_USERNAME_FILE)" > "$OP_ENV_FILE"
+                        && echo "KC_DB_PASSWORD=$(cat $DB_PASSWORD_FILE)" >> "$OP_ENV_FILE"
+                        && echo "KEYCLOAK_ADMIN=$(cat $OP_USERNAME_FILE)" >> "$OP_ENV_FILE"
+                        && echo "KEYCLOAK_ADMIN_PASSWORD=$(cat $OP_PASSWORD_FILE)" >> "$OP_ENV_FILE"
+
+# Write empty key ID to ridt.env file
 [ ! -f "$RIDT_ENV_FILE" ] && echo "KID=" > "$RIDT_ENV_FILE"
 
-echo "OP_HOST=op.localhost" > ".env"
-echo "REALM_NAME=ridt" >> ".env"
+# Write default hostname and default realm name to .env file
+[ ! -f "$RIDT_ENV_FILE" ] && echo "OP_HOST=op.localhost" > $ENV_FILE
+                          && echo "REALM_NAME=ridt" >> $ENV_FILE
