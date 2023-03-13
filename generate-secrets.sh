a1ffa306bf120272464e8521fabca33ee1a3c110
Jonas Primbs <jonas@jonasprimbs.de>
2023-03-13 18:02:04 +0100

    add authentik op2

--- generate-secrets.sh
+++ generate-secrets.sh
@@ -6,7 +6,12 @@ DB_USERNAME_FILE="${SECRETS_DIR}db_username.txt"
 DB_PASSWORD_FILE="${SECRETS_DIR}db_password.txt"
 OP_USERNAME_FILE="${SECRETS_DIR}op_username.txt"
 OP_PASSWORD_FILE="${SECRETS_DIR}op_password.txt"
+PRIVATE_KEY2_FILE="${SECRETS_DIR}certs/private2.pem"
+DB2_USERNAME_FILE="${SECRETS_DIR}db2_username.txt"
+DB2_PASSWORD_FILE="${SECRETS_DIR}db2_password.txt"
+OP2_SECRET_KEY="${SECRETS_DIR}op2_secret_key.txt"
 ICT_ENV_FILE="${SECRETS_DIR}ict.env"
+ICT_ENV2_FILE="${SECRETS_DIR}ict2.env"
 OP_ENV_FILE="${SECRETS_DIR}op.env"
 ENV_FILE=".env"
 
@@ -22,6 +27,9 @@ function generate_secret {
 # Create private key file if not exists
 [ ! -f "$PRIVATE_KEY_FILE" ] && openssl genrsa -out "$PRIVATE_KEY_FILE" 2048
 
+# Create private key 2 file if not exists
+[ ! -f "$PRIVATE_KEY2_FILE" ] && openssl genrsa -out "$PRIVATE_KEY2_FILE" 2048
+
 # Create DB username secret file if not exists
 [ ! -f "$DB_USERNAME_FILE" ] && generate_secret "$DB_USERNAME_FILE" 14
 
@@ -34,6 +42,15 @@ function generate_secret {
 # Create OP password secret file if not exists
 [ ! -f "$OP_PASSWORD_FILE" ] && generate_secret "$OP_PASSWORD_FILE" 64
 
+# Create DB username secret file if not exists
+[ ! -f "$DB2_USERNAME_FILE" ] && generate_secret "$DB2_USERNAME_FILE" 14
+
+# Create DB password secret file if not exists
+[ ! -f "$DB2_PASSWORD_FILE" ] && generate_secret "$DB2_PASSWORD_FILE" 64
+
+# Create secret key for Authentik file if not exists
+[ ! -f "$OP2_SECRET_KEY" ] && generate_secret "$OP2_SECRET_KEY" 64
+
 # Write DB username + password to op.env file
 if [ ! -f "$OP_ENV_FILE" ]
 then
@@ -45,6 +62,9 @@ fi
 
 # Write empty key ID to ict.env file
 [ ! -f "$ICT_ENV_FILE" ] && echo "KID=" > "$ICT_ENV_FILE"
+
+# Write empty key ID to ict2.env file
+[ ! -f "$ICT_ENV2_FILE" ] && echo "KID=" > "$ICT_ENV2_FILE"
 
 # Write default hostname and default realm name to .env file
 if [ ! -f "$ENV_FILE" ]
